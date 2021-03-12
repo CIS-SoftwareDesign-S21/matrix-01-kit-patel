dc2fa5a529d5e65a492038e6dbe346e7bad7afce
Kit <Tuh14411@temple.edu>
2021-03-12 16:37:28 -0500

    adding changes for task 4

--- mmult_omp_timing.c
+++ mmult_omp_timing.c
@@ -17,7 +17,59 @@ int main(int argc, char* argv[]) {
     double *a, *b, *c1, *c2;
     int n;
     double times[2];
+
+    clock_t t;
+
     if (argc > 1) {
+            int max_matrix_size = atoi(argv[1]);
+            FILE *unoptimized_Output, *openMP_Output;
+            unoptimized_Output = fopen("clusterNoSIMD.txt", "w");
+            openMP_Output = fopen("clusterOMP.txt", "w");
+
+            for (int i = 1; i <= max_matrix_size; i++) {
+                n = i;
+                a = gen_matrix(n, n);
+                b = gen_matrix(n, n);
+                c1 = malloc(sizeof(double) * n * n);
+                c2 = malloc(sizeof(double) * n * n);
+
+                t = clock();  // Start time
+                mmult(c1, a, n, n, b, n, n);
+                t = clock() - t;  // End time
+                double time_taken = ((double)t)/CLOCKS_PER_SEC;
+                times[0] = time_taken;  // Time elapsed for non-omp multiplication
+
+
+                t = clock();  // Start time
+                mmult_omp(c2, a, n, n, b, n, n);
+                t = clock() - t;  // End time
+                time_taken = ((double)t)/CLOCKS_PER_SEC;
+                times[1] = time_taken;  // Time elapsed for omp multiplication
+
+                // Stuff we added to log timings for graphing
+                char unoptimized_buffer[256];
+                sprintf(unoptimized_buffer, "%d, %d, %f\n", i, n, times[0]);
+                fwrite(unoptimized_buffer, 1 , strlen(unoptimized_buffer) , unoptimized_Output);
+
+                char omp_buffer[256];
+                sprintf(omp_buffer, "%d, %d, %f\n", i, n, times[1]);
+                fwrite(omp_buffer, 1 , strlen(omp_buffer) , openMP_Output);
+
+                // compare_matrices(c1, c2, n, n);
+            }
+
+            fclose(unoptimized_Output);
+            fclose(openMP_Output);
+        } else {
+            fprintf(stderr, "Usage %s <n>\n", argv[0]);
+        }
+
+
+}
+
+//Old Code
+/*
+if (argc > 1) {
         n = atoi(argv[1]);
         a = gen_matrix(n, n);
         b = gen_matrix(n, n);
@@ -38,5 +90,4 @@ int main(int argc, char* argv[]) {
     } else {
         fprintf(stderr, "Usage %s <n>\n", argv[0]);
     }
-}
-
+*/
