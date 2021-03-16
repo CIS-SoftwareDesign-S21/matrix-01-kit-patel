7e7dcdbfffd07141103f653635629b7178e864b6
Edward Brace <eddiebrace@verizon.net>
2021-03-16 13:12:26 -0400

    again

--- test_mmult.c
+++ test_mmult.c
@@ -55,7 +55,7 @@ int main(int argc, char* argv[])
         
         free( a );
         free( b );
-        free( c );
+        free( c_calc );
         // printf("%f\n", time_taken);
     }//end for()
 
